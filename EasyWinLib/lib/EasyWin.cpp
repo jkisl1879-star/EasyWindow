e05804b82c9a0d3e99ac5781a3d01e134d03e330
zloykin1x <jkisl1879@gmail.com>
2026-06-04 00:10:58 +0300

    Update EasyWin.cpp

--- EasyWinLib/lib/EasyWin.cpp
+++ EasyWinLib/lib/EasyWin.cpp
@@ -1,8 +1,9 @@
 #include "EasyWin.h"
+#include <algorithm>
 
 static EasyWin* currentWindow = nullptr;
 
-// ============ Реализация Button ============
+// ============ Button Implementation ============
 
 Button::Button() : hwnd(nullptr), parent(nullptr), x(0), y(0), width(0), height(0), id(0) {}
 
@@ -22,17 +23,16 @@ void Button::Create(EasyWin* parentWindow, const std::string& buttonText,
     height = h;
     id = buttonId;
 
-    // Конвертируем string в wstring
     std::wstring wtext(buttonText.begin(), buttonText.end());
 
-    hwnd = CreateWindow(
-        L"BUTTON",           // Класс кнопки Windows
-        wtext.c_str(),       // Текст кнопки
-        WS_VISIBLE | WS_CHILD | BS_PUSHBUTTON,  // Стили
-        x, y, w, h,         // Позиция и размер
-        parentWindow->GetHWND(),  // Родительское окно
-        (HMENU)(INT_PTR)buttonId, // ID кнопки
-        GetModuleHandle(nullptr),
+    hwnd = CreateWindowW(
+        L"BUTTON",
+        wtext.c_str(),
+        WS_VISIBLE | WS_CHILD | BS_PUSHBUTTON,
+        x, y, w, h,
+        parentWindow->GetHWND(),
+        (HMENU)(INT_PTR)buttonId,
+        GetModuleHandleW(nullptr),
         nullptr
     );
 }
@@ -40,12 +40,12 @@ void Button::Create(EasyWin* parentWindow, const std::string& buttonText,
 void Button::SetText(const std::string& newText) {
     text = newText;
     std::wstring wtext(newText.begin(), newText.end());
-    SetWindowText(hwnd, wtext.c_str());
+    SetWindowTextW(hwnd, wtext.c_str());
 }
 
 std::string Button::GetText() {
     wchar_t buffer[256];
-    GetWindowText(hwnd, buffer, 256);
+    GetWindowTextW(hwnd, buffer, 256);
     std::wstring wtext(buffer);
     return std::string(wtext.begin(), wtext.end());
 }
@@ -66,119 +66,137 @@ void Button::Disable() {
     EnableWindow(hwnd, FALSE);
 }
 
-void Button::SetClickHandler(std::function<void()> handler) {
-    onClick = handler;
-}
-
-// ============ Реализация EasyWin ============
+// ============ EasyWin Implementation ============
 
 LRESULT CALLBACK EasyWin::WindowProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
-    if (msg == WM_CREATE) {
+    switch (msg) {
+    case WM_NCCREATE: {
         CREATESTRUCT* cs = (CREATESTRUCT*)lParam;
         currentWindow = (EasyWin*)cs->lpCreateParams;
         SetWindowLongPtr(hwnd, GWLP_USERDATA, (LONG_PTR)currentWindow);
+        break;
     }
-    else {
-        currentWindow = (EasyWin*)GetWindowLongPtr(hwnd, GWLP_USERDATA);
     }
 
-    if (currentWindow) {
+    currentWindow = (EasyWin*)GetWindowLongPtr(hwnd, GWLP_USERDATA);
+
+    if (!currentWindow) {
+        return DefWindowProc(hwnd, msg, wParam, lParam);
+    }
+
+    // For fixed window - block all problematic operations
+    if (!currentWindow->resizable) {
         switch (msg) {
-        case WM_PAINT: {
-            PAINTSTRUCT ps;
-            HDC hdc = BeginPaint(hwnd, &ps);
-
-            // Получаем размеры клиентской области
-            RECT clientRect;
-            GetClientRect(hwnd, &clientRect);
-            int clientWidth = clientRect.right - clientRect.left;
-            int clientHeight = clientRect.bottom - clientRect.top;
-
-            // Создаём временный буфер если его нет или изменился размер
-            if (!currentWindow->memDC || clientWidth != currentWindow->backBufferWidth ||
-                clientHeight != currentWindow->backBufferHeight) {
-
-                if (currentWindow->memBitmap) {
-                    DeleteObject(currentWindow->memBitmap);
-                    currentWindow->memBitmap = nullptr;
-                }
-                if (currentWindow->memDC) {
-                    DeleteDC(currentWindow->memDC);
-                    currentWindow->memDC = nullptr;
-                }
-
-                currentWindow->backBufferWidth = clientWidth;
-                currentWindow->backBufferHeight = clientHeight;
-                currentWindow->memDC = CreateCompatibleDC(hdc);
-                currentWindow->memBitmap = CreateCompatibleBitmap(hdc, clientWidth, clientHeight);
-                SelectObject(currentWindow->memDC, currentWindow->memBitmap);
+        case WM_NCHITTEST: {
+            LRESULT result = DefWindowProc(hwnd, msg, wParam, lParam);
+            if (result == HTTOP || result == HTBOTTOM ||
+                result == HTLEFT || result == HTRIGHT ||
+                result == HTTOPLEFT || result == HTTOPRIGHT ||
+                result == HTBOTTOMLEFT || result == HTBOTTOMRIGHT) {
+                return HTCLIENT;
             }
-
-            // Очищаем буфер
-            RECT rect = { 0, 0, currentWindow->backBufferWidth, currentWindow->backBufferHeight };
-            HBRUSH brush = CreateSolidBrush(RGB(240, 240, 240));
-            FillRect(currentWindow->memDC, &rect, brush);
-            DeleteObject(brush);
-
-            // Рисуем в временный буфер
-            currentWindow->currentDC = currentWindow->memDC;
-            currentWindow->OnDraw();
-
-            // Копируем на экран
-            BitBlt(hdc, 0, 0, currentWindow->backBufferWidth, currentWindow->backBufferHeight,
-                currentWindow->memDC, 0, 0, SRCCOPY);
-
-            EndPaint(hwnd, &ps);
-            return 0;
+            return result;
         }
 
-        case WM_SIZE: {
-            int newWidth = LOWORD(lParam);
-            int newHeight = HIWORD(lParam);
-            currentWindow->width = newWidth;
-            currentWindow->height = newHeight;
-            currentWindow->OnResize(newWidth, newHeight);
-
-            // При изменении размера пересоздадим буфер
-            if (currentWindow->memDC) {
-                DeleteDC(currentWindow->memDC);
-                currentWindow->memDC = nullptr;
+        case WM_NCLBUTTONDOWN: {
+            if (wParam == HTCAPTION) {
+                // Start window movement without minimizing
+                SendMessage(hwnd, WM_SYSCOMMAND, SC_MOVE | 0x0002, 0);
+                return 0;
             }
+            break;
+        }
+
+        case WM_SYSCOMMAND: {
+            UINT cmd = wParam & 0xFFF0;
+            if (cmd == SC_MINIMIZE || cmd == SC_MAXIMIZE || cmd == SC_SIZE) {
+                return 0;  // Block
+            }
+            break;
+        }
+        }
+    }
+
+    switch (msg) {
+    case WM_PAINT: {
+        PAINTSTRUCT ps;
+        HDC hdc = BeginPaint(hwnd, &ps);
+
+        RECT clientRect;
+        GetClientRect(hwnd, &clientRect);
+        int clientWidth = clientRect.right - clientRect.left;
+        int clientHeight = clientRect.bottom - clientRect.top;
+
+        if (!currentWindow->memDC || clientWidth != currentWindow->backBufferWidth ||
+            clientHeight != currentWindow->backBufferHeight) {
+
             if (currentWindow->memBitmap) {
                 DeleteObject(currentWindow->memBitmap);
                 currentWindow->memBitmap = nullptr;
             }
-            return 0;
-        }
-
-        case WM_COMMAND: {
-            // Обработка нажатия кнопок
-            int buttonId = LOWORD(wParam);
-            currentWindow->OnButtonClick(buttonId);
-
-            // Вызываем обработчик кнопки если он есть
-            for (auto* btn : currentWindow->buttons) {
-                if (btn->GetId() == buttonId) {
-                    // Можно вызвать лямбду если установлена
-                    break;
-                }
+            if (currentWindow->memDC) {
+                DeleteDC(currentWindow->memDC);
+                currentWindow->memDC = nullptr;
             }
-            return 0;
+
+            currentWindow->backBufferWidth = clientWidth;
+            currentWindow->backBufferHeight = clientHeight;
+            currentWindow->memDC = CreateCompatibleDC(hdc);
+            currentWindow->memBitmap = CreateCompatibleBitmap(hdc, clientWidth, clientHeight);
+            SelectObject(currentWindow->memDC, currentWindow->memBitmap);
         }
 
-        case WM_KEYDOWN:
-            currentWindow->OnKeyDown((int)wParam);
-            return 0;
+        RECT rect = { 0, 0, currentWindow->backBufferWidth, currentWindow->backBufferHeight };
+        HBRUSH brush = CreateSolidBrush(RGB(240, 240, 240));
+        FillRect(currentWindow->memDC, &rect, brush);
+        DeleteObject(brush);
 
-        case WM_MOUSEMOVE:
-            currentWindow->OnMouseMove((int)LOWORD(lParam), (int)HIWORD(lParam));
-            return 0;
+        currentWindow->currentDC = currentWindow->memDC;
+        currentWindow->OnDraw();
 
-        case WM_DESTROY:
-            currentWindow->isRunning = false;
-            PostQuitMessage(0);
-            return 0;
+        BitBlt(hdc, 0, 0, currentWindow->backBufferWidth, currentWindow->backBufferHeight,
+            currentWindow->memDC, 0, 0, SRCCOPY);
+
+        EndPaint(hwnd, &ps);
+        return 0;
+    }
+
+    case WM_SIZE: {
+        int newWidth = LOWORD(lParam);
+        int newHeight = HIWORD(lParam);
+        currentWindow->width = newWidth;
+        currentWindow->height = newHeight;
+        currentWindow->OnResize(newWidth, newHeight);
+
+        if (currentWindow->memDC) {
+            DeleteDC(currentWindow->memDC);
+            currentWindow->memDC = nullptr;
         }
+        if (currentWindow->memBitmap) {
+            DeleteObject(currentWindow->memBitmap);
+            currentWindow->memBitmap = nullptr;
+        }
+        return 0;
+    }
+
+    case WM_COMMAND: {
+        int buttonId = LOWORD(wParam);
+        currentWindow->OnButtonClick(buttonId);
+        return 0;
+    }
+
+    case WM_KEYDOWN:
+        currentWindow->OnKeyDown((int)wParam);
+        return 0;
+
+    case WM_MOUSEMOVE:
+        currentWindow->OnMouseMove((int)LOWORD(lParam), (int)HIWORD(lParam));
+        return 0;
+
+    case WM_DESTROY:
+        currentWindow->isRunning = false;
+        PostQuitMessage(0);
+        return 0;
     }
 
     return DefWindowProc(hwnd, msg, wParam, lParam);
@@ -193,7 +211,8 @@ EasyWin::EasyWin()
     memDC(nullptr),
     memBitmap(nullptr),
     backBufferWidth(0),
-    backBufferHeight(0) {
+    backBufferHeight(0),
+    resizable(true) {
 }
 
 EasyWin::~EasyWin() {
@@ -212,29 +231,72 @@ void EasyWin::Create(const std::string& windowTitle, int w, int h) {
     width = w;
     height = h;
 
-    WNDCLASSEX wc = {};
-    wc.cbSize = sizeof(WNDCLASSEX);
+    WNDCLASSEXW wc = {};
+    wc.cbSize = sizeof(WNDCLASSEXW);
     wc.lpfnWndProc = WindowProc;
-    wc.hInstance = GetModuleHandle(nullptr);
+    wc.hInstance = GetModuleHandleW(nullptr);
     wc.lpszClassName = L"EasyWinClass";
     wc.hbrBackground = nullptr;
     wc.hCursor = LoadCursor(nullptr, IDC_ARROW);
 
-    RegisterClassEx(&wc);
+    RegisterClassExW(&wc);
 
     std::wstring wtitle(windowTitle.begin(), windowTitle.end());
 
-    hwnd = CreateWindowEx(
+    DWORD style;
+    if (resizable) {
+        style = WS_OVERLAPPEDWINDOW;  // Resizable
+    }
+    else {
+        style = WS_OVERLAPPED | WS_CAPTION | WS_SYSMENU | WS_MINIMIZEBOX;  // Fixed size
+    }
+
+    hwnd = CreateWindowExW(
         0,
         L"EasyWinClass",
         wtitle.c_str(),
-        WS_OVERLAPPEDWINDOW,
+        style,
         CW_USEDEFAULT, CW_USEDEFAULT,
         width, height,
         nullptr, nullptr,
-        GetModuleHandle(nullptr),
+        GetModuleHandleW(nullptr),
         this
     );
+}
+
+void EasyWin::SetResizable(bool enable) {
+    resizable = enable;
+
+    if (!hwnd) return;
+
+    LONG_PTR style = GetWindowLongPtr(hwnd, GWL_STYLE);
+
+    if (resizable) {
+        style |= WS_THICKFRAME | WS_MAXIMIZEBOX;
+    }
+    else {
+        style &= ~WS_THICKFRAME;
+        style &= ~WS_MAXIMIZEBOX;
+    }
+
+    SetWindowLongPtr(hwnd, GWL_STYLE, style);
+
+    SetWindowPos(hwnd, nullptr, 0, 0, 0, 0,
+        SWP_NOMOVE | SWP_NOSIZE | SWP_NOZORDER | SWP_FRAMECHANGED);
+}
+
+void EasyWin::SetFixedSize(int w, int h) {
+    width = w;
+    height = h;
+
+    if (hwnd) {
+        RECT rect = { 0, 0, w, h };
+        AdjustWindowRect(&rect, GetWindowLong(hwnd, GWL_STYLE), FALSE);
+        SetWindowPos(hwnd, nullptr, 0, 0, rect.right - rect.left, rect.bottom - rect.top,
+            SWP_NOMOVE | SWP_NOZORDER);
+
+        SetResizable(false);
+    }
 }
 
 void EasyWin::Show() {
@@ -271,11 +333,12 @@ void EasyWin::Redraw() {
     }
 }
 
-// Исправленный AddButton
 Button* EasyWin::AddButton(const std::string& text, int x, int y, int w, int h) {
+    if (!hwnd) return nullptr;
+
     Button* btn = new Button();
     int id = 1000 + (int)buttons.size();
-    btn->Create(this, text, x, y, w, h, id);  // ← this теперь правильный
+    btn->Create(this, text, x, y, w, h, id);
     buttons.push_back(btn);
     return btn;
 }
@@ -285,13 +348,6 @@ void EasyWin::RemoveButton(Button* button) {
     if (it != buttons.end()) {
         delete* it;
         buttons.erase(it);
-    }
-}
-
-void EasyWin::RemoveButton(int index) {
-    if (index >= 0 && index < (int)buttons.size()) {
-        delete buttons[index];
-        buttons.erase(buttons.begin() + index);
     }
 }
 
